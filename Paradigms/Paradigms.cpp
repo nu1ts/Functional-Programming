6e3355b558ba07a6b2c106fde40cdb5a3ef676c9
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-03-06 02:43:44 +0700

    new logic

--- Paradigms/Paradigms.cpp
+++ Paradigms/Paradigms.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <functional>
 #include <algorithm>
+#include <cmath>
+#include <queue>
 using namespace std;
 
 struct Node {
@@ -15,7 +17,7 @@ struct Node {
 
 void stub() { return; }
 
-vector<int> readDataFromFile(const vector<int> const& inputVector) {
+vector<int> readDataFromFile(const vector<int>& inputVector) {
 	vector<int> copiedVector(inputVector);
 
 	int value; cin >> value ? [&]() {
@@ -25,18 +27,38 @@ vector<int> readDataFromFile(const vector<int> const& inputVector) {
 		return copiedVector;
 }
 
-void printElement(const int element) {
-	cout << element << " ";
-}
-
-int getMid(const int& const start, const int& const end) {
+int getMid(const int& start, const int& end) {
 	return (start + end) / 2;
 }
 
-Node* createBalancedTree(const vector<int>& inputVector, const int& start, const int& end, const Node* leftSubtree, const Node* rightSubtree) {
-	Node* node = new Node(getMid(start, end), createBalancedTree(inputVector, start, getMid(start, end) - 1, nullptr, nullptr),
-		createBalancedTree(inputVector, getMid(start, end) + 1, end, nullptr, nullptr));
-	return node;
+Node* merge(Node* root, Node* left, Node* right) {
+	return new Node(root->data, left, right);
+}
+
+Node* createBalancedTree(const vector<int>& inputVector, const int& start, const int& end) { 
+	if (start > end) return nullptr;
+	Node* newNode = new Node(inputVector[getMid(start, end)], nullptr, nullptr);
+	return merge(newNode, createBalancedTree(inputVector, start, getMid(start, end) - 1), createBalancedTree(inputVector, getMid(start, end) + 1, end));
+}
+
+Node* getBalancedTree(const vector<int>& inputVector) { //требуется дебаг
+	return createBalancedTree(inputVector, 0, inputVector.size() - 1);
+}
+
+void BFS(Node* root) { //переписать
+	queue<Node*> q;
+	q.push(root);
+	while (!q.empty()) {
+		Node* cur = q.front();
+		q.pop();
+		cout << cur->data << " ";
+		if (cur->left) {
+			q.push(cur->left);
+		}
+		if (cur->right) {
+			q.push(cur->right);
+		}
+	}
 }
 
 int main()
@@ -44,7 +66,7 @@ int main()
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	vector<int> data = readDataFromFile(vector<int>());
-	createBalancedTree(data, *data.begin(), *data.end(), nullptr, nullptr);
-	for_each(data.begin(), data.end(), printElement);
+	vector<int> nums = readDataFromFile(vector<int>());
+	Node* root = getBalancedTree(nums);
+	BFS(root);
 }
